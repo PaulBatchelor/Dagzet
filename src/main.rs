5550e85f5e332430915142cdd107c9b2cf74239b
Paul Batchelor <thisispaulbatchelor@gmail.com>
2024-07-15 10:11:02 -0400

    added ns command

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,40 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
-fn parse_line(line: &str) {
-    if line.len() < 3 {
-        return;
+struct DagZet {
+    /// The current namespace
+    pub namespace: String,
+}
+
+impl DagZet {
+    pub fn new() -> Self {
+        DagZet {
+            namespace: "".to_string(),
+        }
     }
-    let cmd = &line[0..2];
-    let args = &line[3..];
-    dbg!(cmd, args);
 
-    match cmd {
-        "ns" => {}
-        "gr" => {}
-        "ln" => {}
-        "co" => {}
-        "cr" => {}
-        "nn" => {}
+    pub fn parse_line(&mut self, line: &str) {
+        if line.len() < 3 {
+            return;
+        }
+        let cmd = &line[0..2];
+        let args = &line[3..];
+        dbg!(cmd, args);
 
-        c => {
-            // TODO: (better) error handling
-            panic!("could not find: {c}");
+        match cmd {
+            "ns" => {
+                self.namespace = args.to_string();
+            }
+            "gr" => {}
+            "ln" => {}
+            "co" => {}
+            "cr" => {}
+            "nn" => {}
+
+            c => {
+                // TODO: (better) error handling
+                panic!("could not find: {c}");
+            }
         }
     }
 }
@@ -35,11 +50,26 @@ fn main() {
     let filename: &str = &env::args().last().unwrap();
     let f = File::open(&filename).unwrap();
     let reader = BufReader::new(f);
+    let mut dz = DagZet::new();
 
     //let _ = reader.read_line(&mut line)?;
     let lines_iter = reader.lines().map(|l| l.unwrap());
 
     for str in lines_iter {
-        parse_line(&str);
+        dz.parse_line(&str);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_namespace() {
+        let mut dz = DagZet::new();
+
+        dz.parse_line(&"ns hello");
+
+        assert_eq!(dz.namespace, "hello".to_string());
     }
 }
