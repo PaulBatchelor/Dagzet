ace8c6354516d96f2ee4b25ba2da272ce09e9804
Paul Batchelor <thisispaulbatchelor@gmail.com>
2024-07-15 10:36:47 -0400

    added graph remarks command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
@@ -6,12 +7,14 @@ use std::io::BufReader;
 struct DagZet {
     /// The current namespace
     pub namespace: String,
+    pub graph_remarks: HashMap<String, Vec<String>>,
 }
 
 impl DagZet {
     pub fn new() -> Self {
         DagZet {
             namespace: "".to_string(),
+            graph_remarks: HashMap::new(),
         }
     }
 
@@ -27,7 +30,16 @@ impl DagZet {
             "ns" => {
                 self.namespace = args.to_string();
             }
-            "gr" => {}
+            "gr" => {
+                let gr = &mut self.graph_remarks;
+
+                match gr.get_mut(&self.namespace) {
+                    Some(remarks) => remarks.push(args.to_string()),
+                    None => {
+                        gr.insert(self.namespace.clone(), vec![args.to_string()]);
+                    }
+                }
+            }
             "ln" => {}
             "co" => {}
             "cr" => {}
@@ -71,5 +83,29 @@ mod tests {
         dz.parse_line(&"ns hello");
 
         assert_eq!(dz.namespace, "hello".to_string());
+    }
+
+    #[test]
+    fn test_graph_remarks() {
+        let mut dz = DagZet::new();
+        dz.parse_line(&"ns hello");
+        dz.parse_line(&"gr this is a graph remark");
+        dz.parse_line(&"gr for the node called hello");
+
+        assert_eq!(dz.graph_remarks.len(), 1);
+        assert!(dz.graph_remarks.contains_key("hello"));
+
+        let gr = dz.graph_remarks;
+
+        match gr.get("hello") {
+            Some(remarks) => {
+                assert_eq!(remarks.len(), 2);
+                assert_eq!(remarks[0], "this is a graph remark");
+                assert_eq!(remarks[1], "for the node called hello");
+            }
+            None => {
+                // Shouldn't happen, since there was a check before this
+            }
+        };
     }
 }
