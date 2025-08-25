892e15b7f85b26232e1e36afcbf0d4742fbaf399
Paul Batchelor <thisispaulbatchelor@gmail.com>
2025-08-25 19:43:41 -0400

    boilerplate file IO

--- src/bin/logzet.rs
+++ src/bin/logzet.rs
@@ -1,5 +1,33 @@
 use dagzet::logzet;
+use std::env;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::io::BufReader;
 
 fn main() {
     logzet::hello();
+    let mut stdin = false;
+
+    if env::args().len() < 2 {
+        stdin = true;
+    }
+
+    if stdin {
+        let reader = BufReader::new(io::stdin());
+        for line in reader.lines().map_while(Result::ok) {
+            println!("{}", line);
+        }
+        return;
+    }
+
+    let filenames = env::args().skip(1);
+
+    for filename in filenames {
+        let f = File::open(filename).unwrap();
+        let reader = BufReader::new(f);
+        for line in reader.lines().map_while(Result::ok) {
+            println!("{}", line);
+        }
+    }
 }
